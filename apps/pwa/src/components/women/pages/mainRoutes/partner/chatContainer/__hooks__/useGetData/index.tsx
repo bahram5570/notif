6a87eb0b2e7be33acadd2da091cbf7e1012e59bf
i.mortezazadeh <i.mortezazadeh@MGMT-MORTEZA>
2025-08-18 11:01:06 +0330

    create 404 page

--- apps/pwa/src/components/women/pages/mainRoutes/partner/chatContainer/__hooks__/useGetData/index.tsx
+++ apps/pwa/src/components/women/pages/mainRoutes/partner/chatContainer/__hooks__/useGetData/index.tsx
@@ -1,13 +1,25 @@
+import { useEffect } from 'react';
+
 import useApi from '@hooks/useApi';
+import { useRouter } from 'next/navigation';
 
 import { ResponsePropsType, UseGetDataPropsType } from './type';
 
 const useGetData = ({ id }: UseGetDataPropsType) => {
+  const router = useRouter();
+
   const { data, isLoading } = useApi<ResponsePropsType>({
     api: `challenge/${id}/chat`,
     method: 'GET',
     queryKey: [`chat-${id}`],
   });
+
+  useEffect(() => {
+    if (!isLoading && !data) {
+      router.replace('/not-found');
+    }
+  }, [isLoading, data]);
+
   return { data, isLoading };
 };
 
--- apps/pwa/src/components/women/pages/nestedRoutes/profile/support/SupportTicketDetailPage/__hooks__/useGetData/index.tsx
+++ apps/pwa/src/components/women/pages/nestedRoutes/profile/support/SupportTicketDetailPage/__hooks__/useGetData/index.tsx
@@ -1,16 +1,25 @@
+import { useEffect } from 'react';
+
 import useApi from '@hooks/useApi';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
 import { ChatResponseTypes } from './type';
 
 const useGetData = () => {
   const { ticketId } = useParams();
+  const router = useRouter();
 
   const { data, isLoading } = useApi<ChatResponseTypes>({
     api: `support/ticket/${ticketId}`,
     method: 'GET',
     queryKey: [`ticketDetail`],
   });
+
+  useEffect(() => {
+    if (!isLoading && !data) {
+      router.replace('/not-found');
+    }
+  }, [isLoading, data]);
 
   return { data, isLoading };
 };
