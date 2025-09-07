43611272fa0550e2908b545f5a82f10031e4a3bc
bahram <bahramsa70@gmail.com>
2025-09-07 15:35:53 +0330

    debug

--- apps/pwa/src/constants/app.constants.ts
+++ apps/pwa/src/constants/app.constants.ts
@@ -1,4 +1,3 @@
 export const PORTAL_ID = 'portalId';
 export const MAX_SCREEN_WIDTH = 480;
-// export const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION || '';
-export const APP_VERSION = '5.10.0 PWA WEBSITE';
+export const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION || '';
--- apps/web/src/hooks/useUserTracking/index.tsx
+++ apps/web/src/hooks/useUserTracking/index.tsx
@@ -2,34 +2,6 @@ import { getUserTracking } from '@providers/UserTrackingProvider/__utils__';
 
 import useApi from '@hooks/useApi';
 
-// const useUserTracking = () => {
-//   const [userTrackingData, setUserTrackingData] = useState<UserTrackingTypes | null>(null);
-
-//   useEffect(() => {
-//     const data = getUserTracking();
-
-//     if (data) {
-//       setUserTrackingData(data);
-//     }
-//   }, []);
-
-//   const { callApi } = useApi({ url: 'support/attribution', method: 'POST' });
-
-//   const callUserTracking = (downloadLink: string) => {
-//     if (userTrackingData) {
-//       const payload = {
-//         downloadLink,
-//         ip: userTrackingData.ip,
-//         urls: userTrackingData.urls,
-//         username: userTrackingData.username,
-//         userAgent: userTrackingData.userAgent,
-//         macAddress: userTrackingData.macAddress,
-//       };
-
-//       callApi(payload);
-//     }
-//   };
-
 const useUserTracking = () => {
   const { callApi } = useApi({ url: 'support/attribution', method: 'POST' });
 
