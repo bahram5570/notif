3acb2199a577c1edd382c84d36b1b16e4b8be7ae
mohammadsarabi <sarabim395@gmail.com>
2025-09-23 12:37:00 +0330

    xiaomiWidget add

---
+++ apps/web/src/components/pages/webView/XiaomiWidgetContainer/constants.ts
@@ -0,0 +1,27 @@
+export const ANDROIDWIDGETDATA = [
+  {
+    id: 1,
+    title: 'بعد از لمس و مکث روی صفحه گوشی، گزینه "widgets" که پایین صفحه برات نمایش داده میشه رو انتخاب کن',
+    image: '/assets/images/webView/widgetXiaomi/1.webp',
+  },
+  {
+    id: 2,
+    title: 'توی این مرحله باید قسمت Apps with multiple widgets رو پیدا کنی و روی دکمه "All" بزنی.',
+    image: '/assets/images/webView/widgetXiaomi/2.webp',
+  },
+  {
+    id: 3,
+    title: 'حالا برو اخر لیست و روی گزینه  "Android widgets" بزنی.',
+    image: '/assets/images/webView/widgetXiaomi/3.webp',
+  },
+  {
+    id: 4,
+    title: 'توی این صفحه اپلیکیشن ایمپو رو پیدا کن و براساس هدف نصبت، روی یکی از ویجت‌ها کلیک کن و دکمه "add" رو بزن ',
+    image: '/assets/images/webView/widgetXiaomi/4.webp',
+  },
+  {
+    id: 5,
+    title: 'حالا می‌تونی کارت ویجت رو بصورت زیر روی صفحه اصلی گوشیت ببینی',
+    image: '/assets/images/webView/widgetXiaomi/5.webp',
+  },
+];
