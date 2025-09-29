271c55d20af73f2869cd851589861661df941443
NazaninShahidi <NazaninShahidi298@gmail.com>
2025-09-29 13:27:21 +0330

    use handleKeyDown

--- apps/pwa/src/components/women/pages/nestedRoutes/profile/UserInfoPage/UserInfoName/index.tsx
+++ apps/pwa/src/components/women/pages/nestedRoutes/profile/UserInfoPage/UserInfoName/index.tsx
@@ -1,17 +1,29 @@
+import { useRef } from 'react';
+
 import useTheme from '@hooks/useTheme';
 
 import { UserInfoNameTypes } from './types';
 
 const UserInfoName = ({ value, valueHandler }: UserInfoNameTypes) => {
   const { colors, typography } = useTheme();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      inputRef.current?.blur();
+    }
+  };
 
   return (
     <input
+      ref={inputRef}
       type="text"
       value={value}
       className="w-full"
       onChange={(v) => valueHandler(v.target.value)}
       style={{ color: colors.Surface_OnSurfaceVariant, ...typography.Body.Large }}
+      onKeyUp={handleKeyDown}
     />
   );
 };
