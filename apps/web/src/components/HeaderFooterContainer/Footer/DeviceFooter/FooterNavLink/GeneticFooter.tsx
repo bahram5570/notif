00ffcd430182b22273580f36736ccf47234ed430
mohammadsarabi <sarabim395@gmail.com>
2025-10-11 08:38:50 +0330

    GeneticFooter change

--- apps/web/src/components/HeaderFooterContainer/Footer/DeviceFooter/FooterNavLink/GeneticFooter.tsx
+++ apps/web/src/components/HeaderFooterContainer/Footer/DeviceFooter/FooterNavLink/GeneticFooter.tsx
@@ -1,11 +1,19 @@
 import CustomImage from '@components/ui/CustomImage';
 import CustomLink from '@components/ui/CustomLink';
 import { FOOTER_HEIGHT } from '@constants/app.constants';
+import { usePathname } from 'next/navigation';
 
 const GeneticFooter = () => {
+  const pathname = usePathname();
+  const list = ['/landing/iran-girl-name', '/landing/iran-boy-name', '/landing/girl-name', '/landing/boy-name'];
+
+  const shouldShowFooter = list.some((item) => pathname?.includes(item));
+
+  if (shouldShowFooter) return null;
+
   return (
-    <CustomLink className="fixed left-4 z-15" style={{ bottom: FOOTER_HEIGHT }} href={'landing/geneticMarketing'}>
-      <CustomImage src={'/assets/images/geneticMarketing/genetic-footer-logo.webp'} alt="" width={120} height={120} />
+    <CustomLink className="fixed right-4 z-20" style={{ bottom: FOOTER_HEIGHT }} href="/landing/geneticMarketing">
+      <CustomImage src="/assets/images/geneticMarketing/genetic-footer-logo.webp" alt="" width={120} height={120} />
     </CustomLink>
   );
 };
