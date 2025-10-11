f0f500f5530a74830cd4b8761ad88e59fe5d37c7
mohammadsarabi <sarabim395@gmail.com>
2025-10-11 16:03:09 +0330

    remove log useAnalytics

--- apps/pwa/src/hooks/useAnalytics/index.tsx
+++ apps/pwa/src/hooks/useAnalytics/index.tsx
@@ -9,8 +9,6 @@ import useAnalyticsPageView from './useAnalyticsPageView';
 
 const useAnalytics = (props?: UseAnalyticsTypes) => {
   const callEvent: UseAnalyticsCallEventTypes = useCallback((event_name, properties) => {
-    console.log(event_name);
-
     posthog.capture(event_name, properties);
   }, []);
 
