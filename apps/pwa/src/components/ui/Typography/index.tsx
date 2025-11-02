d08f4583abe46ad66823cdfbd689b0604f805238
NazaninShahidi <NazaninShahidi298@gmail.com>
2025-11-02 16:56:27 +0330

    add skeleton

--- apps/pwa/src/components/ui/Typography/index.tsx
+++ apps/pwa/src/components/ui/Typography/index.tsx
@@ -36,6 +36,11 @@ const Typography = (props: TypographyProps) => {
       break;
   }
 
+  let result = props.toEnglishNumber
+    ? toEnglishNumbers(props?.children || '')
+    : toPersianNumbers(props?.children || '');
+
+  result = result.replace(/\\n/g, '\n');
   return (
     <>
       {props.children && (
@@ -50,7 +55,7 @@ const Typography = (props: TypographyProps) => {
             textAlign: props.textAlign,
           }}
         >
-          {props.toEnglishNumber ? toEnglishNumbers(props.children) : toPersianNumbers(props.children)}
+          {result}
         </p>
       )}
     </>
--- apps/pwa/src/components/women/pages/nestedRoutes/AiTopicsChatbotPage/WelcomingContainer/TopicsChatbotWelcoming/index.tsx
+++ apps/pwa/src/components/women/pages/nestedRoutes/AiTopicsChatbotPage/WelcomingContainer/TopicsChatbotWelcoming/index.tsx
@@ -1,4 +1,3 @@
-import Icon from '@assets/icons/Generate B.svg';
 import { removeWelcomingHandler } from '@utils/aiChatBotWelcoming';
 import { colorFormatConverter } from '@utils/scripts';
 
@@ -7,6 +6,7 @@ import Typography from '@components/ui/Typography';
 import { MAX_SCREEN_WIDTH } from '@constants/app.constants';
 import useWidgetActions from '@hooks/useWidgetActions';
 
+import TopicsChatbotWelcomingSkeleton from './TopicsChatbotWelcomingSkeleton';
 import useGetData from './__hooks__/useGetData';
 
 const TopicsChatbotWelcoming = ({ top, onUpdateKeyHandler }: { top: number; onUpdateKeyHandler: any }) => {
@@ -17,38 +17,45 @@ const TopicsChatbotWelcoming = ({ top, onUpdateKeyHandler }: { top: number; onUp
     // callEvent('ChatbotInteractionClick');
     if (data) {
       actionHandler(data.button.action);
-      removeWelcomingHandler();
-      onUpdateKeyHandler();
+
+      setTimeout(() => {
+        removeWelcomingHandler();
+        onUpdateKeyHandler();
+      }, 3000);
     }
   };
 
   return (
     <div className="absolute mx-auto   left-0 right-0 bottom-0 px-5 " style={{ maxWidth: MAX_SCREEN_WIDTH, top }}>
-      <div className="px-6 flex flex-col gap-14  glass-card shadow-lg rounded-[32px] pt-6 pb-8 ">
-        <div className=" flex flex-col gap-3">
-          <div className="flex flex-col justify-end items-end gap-1">
-            <Typography scale="Title" size="Large">
-              {data?.title}
-            </Typography>
-          </div>
-          <Typography scale="Body" size="Medium" textAlign="justify">
-            {data?.description}
-          </Typography>
-        </div>
-
-        <Button
-          variant="fill"
-          size="medium"
-          color="FREE-STYLES"
-          contentsColor={colorFormatConverter(data?.button.foregroundColor || '')}
-          buttonColor={colorFormatConverter(data?.button.backgroundColor || '')}
-          onClick={onClick}
-          fullWidth
-        >
-          <Typography scale="Lable" size="Large" color="White">
-            {data?.button.text}
-          </Typography>
-        </Button>
+      <div className="px-6 flex flex-col gap-12  glass-card shadow-lg rounded-[32px] pt-6 pb-8 ">
+        {isLoading && <TopicsChatbotWelcomingSkeleton />}
+        {!isLoading && (
+          <>
+            <div className=" flex flex-col gap-3">
+              <div className="flex flex-col justify-end items-end gap-1">
+                <Typography scale="Title" size="Large">
+                  {data?.title}
+                </Typography>
+              </div>
+              <Typography scale="Body" size="Medium" textAlign="justify">
+                {data?.description}
+              </Typography>
+            </div>
+            <Button
+              variant="fill"
+              size="large"
+              color="FREE-STYLES"
+              contentsColor={colorFormatConverter(data?.button.foregroundColor || '')}
+              buttonColor={colorFormatConverter(data?.button.backgroundColor || '')}
+              onClick={onClick}
+              fullWidth
+            >
+              <Typography scale="Lable" size="Large" color="White">
+                {data?.button.text}
+              </Typography>
+            </Button>
+          </>
+        )}
       </div>
     </div>
   );
