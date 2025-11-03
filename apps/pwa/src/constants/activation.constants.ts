e19dda792ef06572c6e2ee0249c0e602fe01a199
bahram <bahramsa70@gmail.com>
2025-11-03 09:30:49 +0330

    update ACTIVATION_HEIGHT_VALUES

--- apps/pwa/src/constants/activation.constants.ts
+++ apps/pwa/src/constants/activation.constants.ts
@@ -172,4 +172,4 @@ export enum GoalNamesEnum {
 }
 
 export const ACTIVATION_WEIGHT_VALUES = { min: 20, max: 200, defaultValue: 70, unit: 'kg' };
-export const ACTIVATION_HEIGHT_VALUES = { min: 100, max: 220, defaultValue: 160, unit: 'cm' };
+export const ACTIVATION_HEIGHT_VALUES = { min: 100, max: 220, defaultValue: 150, unit: 'cm' };
