4bccabae330913e459b2ceebf7436005ac8cb87c
mohammadsarabi <sarabim395@gmail.com>
2025-12-21 16:27:51 +0330

    add color

--- apps/web/src/components/pages/landing/ClinicPage/ClinicHeading/index.tsx
+++ apps/web/src/components/pages/landing/ClinicPage/ClinicHeading/index.tsx
@@ -3,20 +3,16 @@ import CustomTypography from '@components/ui/CustomTypography';
 
 const ClinicHeading = () => {
   return (
-    <div className="mx-[16px] mt-4 grid [grid-template-columns:1fr_40%] items-center justify-between bg-pink-50 px-[16px] lg:px-0 rounded-xl lg:rounded-3xl lg:h-72">
+    <div className="mx-[16px] mt-4 grid [grid-template-columns:1fr_40%] items-center justify-between !bg-impo_Pink_50 px-[16px] lg:px-0 rounded-xl lg:rounded-3xl lg:h-72">
       <div className="py-[16px] lg:pr-10 lg:mb-14">
         <h1>
-          <CustomTypography
-            fontSize="Title_Small"
-            className="mb-2 lg:block !text-impo_Neutral_OnBackground"
-            tagType="span"
-          >
+          <CustomTypography fontSize="Title_Small" className="mb-2 lg:block " tagType="span">
             ویزیت دکتر آنلاین؛
           </CustomTypography>
-          <CustomTypography fontSize="Title_Small" tagType="span" className="!text-impo_Neutral_OnBackground">
+          <CustomTypography fontSize="Title_Small" tagType="span">
             مشاوره پزشکی سریع،تخصصی و مطمئن در
           </CustomTypography>
-          <CustomTypography fontSize="Title_Medium" tagType="span" className="!text-impo_Neutral_OnBackground">
+          <CustomTypography fontSize="Title_Medium" tagType="span">
             ایمپو
           </CustomTypography>
         </h1>
