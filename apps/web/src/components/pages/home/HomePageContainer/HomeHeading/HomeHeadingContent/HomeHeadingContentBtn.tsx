9bf4d957b2cb5edf9561b5d7c5cbcf3eac0f6141
NazaninShahidi <NazaninShahidi298@gmail.com>
2025-12-23 07:44:17 +0330

    update image

--- apps/web/src/components/pages/home/HomePageContainer/HomeHeading/HomeHeadingContent/HomeHeadingContentBtn.tsx
+++ apps/web/src/components/pages/home/HomePageContainer/HomeHeading/HomeHeadingContent/HomeHeadingContentBtn.tsx
@@ -11,9 +11,12 @@ const HomeHeadingContentBtn = () => {
 
   return (
     <CustomButton
-      varient="fill"
+      varient="FREE_COLORS"
       onClick={() => scrollToIdHandler(DOWNLOAD_APP_SECTION_ID)}
       fontSize={breakPoint.laptop ? 'Lable_Large' : 'Title_Small'}
+      backgroundColor=""
+      borderColor=""
+      textColor=""
       className="!w-[120px] lg:!w-[182px] h-[40px] !mt-2 !mb-8 !bg-impo_Primary_Primary !border-impo_Primary_Primary !text-impo_Primary_OnPrimary"
       id="install_home"
     >
