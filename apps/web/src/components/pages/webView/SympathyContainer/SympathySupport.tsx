ac78e4246b5694dd69da676feaf194e523b61710
NazaninShahidi <NazaninShahidi298@gmail.com>
2026-01-11 15:40:09 +0330

    change sympaty

--- apps/web/src/components/pages/webView/SympathyContainer/SympathySupport.tsx
+++ apps/web/src/components/pages/webView/SympathyContainer/SympathySupport.tsx
@@ -5,12 +5,12 @@ import ContentCardContainer from '@components/ui/WebView/ContentCardContainer';
 const SympathySupport = () => {
   return (
     <div className="flex flex-col  gap-4">
-      <CustomTypography fontSize="Title_Small" className="!text-impo_Neutral_OnBackground">
+      <CustomTypography fontSize="Title_Small" className="!text-impo_Black">
         هر روز حس حمایت و توجه رو تجربه کن 🤗
       </CustomTypography>
 
       <ContentCardContainer bgUrl="/assets/images/webView/sympathy/bg-2.webp">
-        <CustomTypography fontSize="Body_Medium" className="px-3 pt-4 !text-impo_Neutral_OnBackground">
+        <CustomTypography fontSize="Body_Medium" className="px-3 pt-4 !text-impo_Black">
           ایمپو بهت کمک می‌کنه تا پارتنرت از حالت با خبر باشه و رابطه‌تون هر روز قشنگ‌تر بشه
         </CustomTypography>
         <CustomImage
@@ -22,7 +22,7 @@ const SympathySupport = () => {
         />
       </ContentCardContainer>
       <ContentCardContainer bgUrl="/assets/images/webView/sympathy/bg-3.webp">
-        <CustomTypography fontSize="Body_Medium" className="px-3 pt-4 !text-impo_Neutral_OnBackground">
+        <CustomTypography fontSize="Body_Medium" className="px-3 pt-4 !text-impo_Black ">
           ایمپو روزهای پریود , PMS و تخمک‌گذاریت رو به پارتنرت خبر میده تا هم هوات رو بیشتر داشته باشه هم برای باهم بودن
           برنامه‌ریزی کنید.
         </CustomTypography>
@@ -36,7 +36,7 @@ const SympathySupport = () => {
       </ContentCardContainer>
 
       <ContentCardContainer bgUrl="/assets/images/webView/sympathy/bg-1.webp">
-        <CustomTypography fontSize="Body_Medium" className="px-3 pt-4 !text-impo_Neutral_OnBackground">
+        <CustomTypography fontSize="Body_Medium" className="px-3 pt-4 !text-impo_Black">
           کلی برنامه مشترک برای اینکه رابطه‌تو بهتر بشه و هیچ مشکلی پیش نیاد
         </CustomTypography>
         <CustomImage
