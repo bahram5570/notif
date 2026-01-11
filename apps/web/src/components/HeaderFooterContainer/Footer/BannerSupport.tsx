be03c197ed6f4cb14becc04b9e44f0eb008252ec
mohammadsarabi <sarabim395@gmail.com>
2026-01-11 14:50:47 +0330

    remove opacity

--- apps/web/src/components/HeaderFooterContainer/Footer/BannerSupport.tsx
+++ apps/web/src/components/HeaderFooterContainer/Footer/BannerSupport.tsx
@@ -5,7 +5,7 @@ const BannerSupport = () => {
   return (
     <CustomLink
       href={'tel:09157097500'}
-      className="flex items-center justify-center w-full h-20 py-4 px-3 bg-impo_Pink_500 !text-impo_White z-10 text-center fixed left-0 right-0 bottom-0"
+      className="flex items-center justify-center w-full h-20 py-4 px-3 bg-impo_Pink_500 !text-impo_White z-10 text-center fixed left-0 right-0 bottom-0 hover:opacity-[1]"
     >
       <CustomTypography fontSize="Lable_Large">
         در صورت مشکل در فرآیند ورود یا ثبت نام از ساعت ۸ الی ۲۲ با شماره 09157097500 تماس بگیرید
