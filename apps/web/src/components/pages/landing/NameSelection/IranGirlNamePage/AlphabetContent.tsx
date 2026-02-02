1cce3bf73a4ed01f80d0fb95abc22567ba64895d
mohammadsarabi <sarabim395@gmail.com>
2026-02-02 16:01:29 +0330

    update content

--- apps/web/src/components/pages/landing/NameSelection/IranGirlNamePage/AlphabetContent.tsx
+++ apps/web/src/components/pages/landing/NameSelection/IranGirlNamePage/AlphabetContent.tsx
@@ -4,15 +4,18 @@ const AlphabetContent = () => {
   return (
     <>
       اگه تو دوران
-      <CustomLink className="!text-impo_Primary_Primary" href="/pregnancy">
+      <CustomLink className="!text-impo_Primary_Primary mx-[3px] inline-block" href="/pregnancy">
         بارداری
       </CustomLink>
       هستی و سونوگرافی یا
-      <CustomLink className="!text-impo_Primary_Primary" href="/pregnancy-symptoms-of-having-a-girl">
+      <CustomLink
+        className="!text-impo_Primary_Primary mx-[3px] inline-block"
+        href="/pregnancy-symptoms-of-having-a-girl"
+      >
         علائم صد در صد جنین دختر
       </CustomLink>
       بهت خبر دادن که قراره یک فرشته کوچولوی خوشگل داشته باشی، پس حتما انتخاب یک
-      <CustomLink className="!text-impo_Primary_Primary" href="/landing/girl-name">
+      <CustomLink className="!text-impo_Primary_Primary mx-[3px] inline-block" href="/landing/girl-name">
         اسم دختر
       </CustomLink>
       ایرانی خاص ذهن خودت و اطرافیانتو حسابی درگیر کرده. یادت نره که مرور اسم‌های دخترانه ایرانی شیک بر اساس حروف الفبا
