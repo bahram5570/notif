a94b939c5ea12627e56485e3818d4a481a3b8817
mohammadsarabi <sarabim395@gmail.com>
2026-03-03 09:49:01 +0330

    HEADER_LINKS_LIST update

--- apps/web/src/constants/links.constants.ts
+++ apps/web/src/constants/links.constants.ts
@@ -59,7 +59,7 @@ export const HEADER_LINKS_LIST: HEADER_LINKS_TYPES = [
       { title: 'محاسبه سن بارداری و تقویم  بارداری', linkTo: '/calendarOfPregnancy' },
       { title: 'تست حاملگی آنلاین', linkTo: '/landing/pregnancy' },
       { title: 'محاسبه آنلاین تخمک گذاری', linkTo: '/ovulation-calculator' },
-      { title: 'محاسبه آنلاین سیکل قاعدگی', linkTo: '/menstrual-cycle' },
+      { title: 'محاسبه آنلاین سیکل قاعدگی', linkTo: '/period-calculator' },
       { title: 'ابزار انتخاب اسم دختر', linkTo: '/landing/girl-name' },
       { title: 'ابزار انتخاب اسم پسر', linkTo: '/landing/boy-name' },
       { title: 'تست ژنتیک قبل از بارداری', linkTo: '/landing/genetic-screening-before-pregnancy' },
