4220d015c1c6cbf6be9e6ae6819dd6ce47e8f92e
NazaninShahidi <nazaninshahidi298@gmail.com>
2026-04-07 10:44:05 +0330

    create clinicBanner and article List

---
+++ apps/men-pwa/src/components/pages/mainRoutes/__knowledge__/KnowledgePage/ArticleList/ArticleItem/index.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+
+import { CustomImage } from '@repo/core/components/ui/CustomImage';
+import { CustomTypography } from '@repo/core/components/ui/CustomTypography';
+import { gregorianFarsiScriptDate, jalaaliScriptDate } from '@repo/core/utils/dates';
+
+import { useCulture } from '@repo/core/hooks/useCulture';
+import { useWidgetActions } from '@repo/core/hooks/useWidgetActions';
+import { CalendarTypeEnum } from '@repo/core/providers/CultureProvider';
+import { ActionTypeEnum, ActionTypes } from '@repo/core/providers/WidgetActionsProvider';
+import moment from 'moment-jalaali';
+
+import { ArticleItemPropsType } from './type';
+
+const ArticleItem = (props: ArticleItemPropsType) => {
+  const { culture } = useCulture();
+  const { actionHandler } = useWidgetActions();
+  const [currentDate, setCurrentDate] = useState('');
+
+  const linkToHandler = () => {
+    const action: ActionTypes = {
+      api: '',
+      internal: '',
+      actionTypes: [],
+      actionType: ActionTypeEnum.None,
+      nextStep: { type: 0, isDismissible: false, delaySecond: 0, data: null },
+    };
+
+    action.actionType = ActionTypeEnum.ExternalRout;
+    action.api = props.link2;
+
+    actionHandler(action);
+  };
+
+  useEffect(() => {
+    const date = new Date(props.pubDate);
+    if (culture.calendarType === CalendarTypeEnum.Gregorian) {
+      const gregorianDate = moment(date).format('YYYY-MM-DD');
+      const gregorianFarsMonth = gregorianFarsiScriptDate(gregorianDate);
+
+      setCurrentDate(gregorianFarsMonth);
+    }
+
+    if (culture.calendarType === CalendarTypeEnum.Jalali) {
+      const jalaaliMoment = moment(date).format('jYYYY/jMM/jDD');
+      const jalaaliMonth = jalaaliScriptDate(jalaaliMoment);
+
+      setCurrentDate(jalaaliMonth);
+    }
+  }, [culture.calendarType]);
+
+  return (
+    <div className="flex flex-col gap-1" onClick={linkToHandler}>
+      <CustomImage src={JSON.parse(props.description)} className="w-[200px] h-[112px] rounded-md" />
+      <div className="flex flex-col items-end">
+        <CustomTypography fontSize="Lable_Medium">{props.title}</CustomTypography>
+        <CustomTypography fontSize="Body_Small" className="text-impo_Surface_Outline">
+          {currentDate}
+        </CustomTypography>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleItem;
--- apps/men-pwa/src/components/pages/mainRoutes/__knowledge__/KnowledgePage/index.tsx
+++ apps/men-pwa/src/components/pages/mainRoutes/__knowledge__/KnowledgePage/index.tsx
@@ -3,6 +3,10 @@
 import { MainPageLayout } from '@repo/core/components/MainPageLayout';
 import { WidgetGenerators } from '@repo/core/components/Widgets/WidgetGenerators';
 
+import { FOOTER_HEIGHT } from '@repo/core/constants/app.constants';
+
+import ArticleList from './ArticleList';
+import ClinicBanner from './ClinicBanner';
 import useGetKnowledgeData from './__hooks__/useGetKnowledgeData';
 
 const KnowledgePage = () => {
@@ -12,7 +16,9 @@ const KnowledgePage = () => {
     <MainPageLayout leftElement1="Profile">
       {isLoading && !data && <div></div>}
       {!isLoading && data && (
-        <div className="flex flex-col gap-6 min-h-[100dvh]">
+        <div className="flex flex-col gap-6 min-h-[100dvh] px-4" style={{ paddingBottom: FOOTER_HEIGHT + 20 }}>
+          <ArticleList items={data.items} />
+          <ClinicBanner />
           <div className="flex flex-col">
             {data.widgets.map((widget, index) => (
               <WidgetGenerators {...widget} key={index} />
