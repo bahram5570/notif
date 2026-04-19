0f9981b26b5ae4ef88318e450fd80ecdf730fafd
NazaninShahidi <nazaninshahidi298@gmail.com>
2026-04-19 16:35:27 +0330

    fix  toggle play

--- packages/core/src/components/ui/VoiceMessagePlayer/VoiceMessagePlayer.tsx
+++ packages/core/src/components/ui/VoiceMessagePlayer/VoiceMessagePlayer.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 
 import PauseIcon from '@assets/shared/icons/Pause.svg';
 import PlayIcon from '@assets/shared/icons/Play.svg';
@@ -6,6 +6,7 @@ import { textShorter } from '@repo/core/utils/scripts';
 
 import { useSystem } from '../../../hooks/useSystem';
 import useFetchAudio from './__hooks__/useFetchAudio';
+import { registerAndPlay } from './audioManager';
 import { VoiceMessagePlayerPropsType } from './type';
 
 export const VoiceMessagePlayer = ({ voiceUrl }: VoiceMessagePlayerPropsType) => {
@@ -13,16 +14,34 @@ export const VoiceMessagePlayer = ({ voiceUrl }: VoiceMessagePlayerPropsType) =>
   const { audioSrc } = useFetchAudio({ voiceUrl });
   const [isPlaying, setIsPlaying] = useState(false);
   const { appName } = useSystem();
-  const isMan = appName === 'MEN_PWA';
 
   const togglePlay = () => {
-    if (!audioRef.current) return;
+    const audio = audioRef.current;
+    if (!audio) return;
+
     if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
+      audio.pause();
+      return;
     }
+
+    registerAndPlay(audio);
   };
+
+  const theme = useMemo<{ backgroundcolor: string; fill: string }>(() => {
+    switch (appName) {
+      case 'MEN_PWA':
+        return {
+          backgroundcolor: 'bg-impo_PrimaryMan_PrimaryContainerMan',
+          fill: 'fill-impo_PrimaryMan_PrimaryMan',
+        };
+
+      default:
+        return {
+          backgroundcolor: 'bg-impo_Primary_PrimaryContainer',
+          fill: 'fill-impo_Primary_Primary',
+        };
+    }
+  }, [appName]);
 
   const name = textShorter(voiceUrl, 24);
 
@@ -31,16 +50,12 @@ export const VoiceMessagePlayer = ({ voiceUrl }: VoiceMessagePlayerPropsType) =>
       <button
         disabled={!audioSrc}
         onClick={togglePlay}
-        className={`rounded-full w-12 h-12 flex justify-center items-center  ${isMan ? 'bg-impo_PrimaryMan_PrimaryContainerMan' : 'bg-impo_Primary_PrimaryContainer'}`}
+        className={`rounded-full w-12 h-12 flex justify-center items-center  ${theme.backgroundcolor}`}
       >
         {isPlaying ? (
-          <PauseIcon
-            className={`w-5 h-auto  ${isMan ? 'fill-impo_PrimaryMan_PrimaryMan' : 'fill-impo_Primary_Primary'}`}
-          />
+          <PauseIcon className={`w-5 h-auto  ${theme.fill}`} />
         ) : (
-          <PlayIcon
-            className={`w-5 h-auto  ${isMan ? 'fill-impo_PrimaryMan_PrimaryMan' : 'fill-impo_Primary_Primary'}`}
-          />
+          <PlayIcon className={`w-5 h-auto  ${theme.fill}`} />
         )}
       </button>
 
