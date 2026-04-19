532a0dd56e46f84a34f363c1b34b2a35eba37e0b
NazaninShahidi <nazaninshahidi298@gmail.com>
2026-04-19 09:01:34 +0330

    fix import of type

--- apps/pwa/src/components/pages/mainRoutes/clinic/ClinicDoctorInfoContainer/DoctorInfo/types.d.ts
+++ apps/pwa/src/components/pages/mainRoutes/clinic/ClinicDoctorInfoContainer/DoctorInfo/types.d.ts
@@ -1,4 +1,4 @@
-import { ClinicInfoResponseTypes } from '../../ClinicInfoContainer/__hooks__/useGetDataClinicInfo/types';
+import { ClinicInfoResponseTypes } from '@repo/core/components/clinic';
 
 type DrTypes = Pick<ClinicInfoResponseTypes, 'info'>['info']['dr'][0];
 type InfoTypes = Pick<DrTypes, 'firstName' | 'lastName' | 'image' | 'nezamNumber' | 'speciliaty'>;
