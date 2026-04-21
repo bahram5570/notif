7dee370af5dc738f931d6d7563115f4edeb84101
bahram <bahramsa70@gmail.com>
2026-04-21 13:53:58 +0330

    update manifest images

--- apps/pwa/src/app/manifest.ts
+++ apps/pwa/src/app/manifest.ts
@@ -11,34 +11,34 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#FFFFFF',
     icons: [
       {
-        src: '/assets/manifest/impo.webp',
+        src: '/assets/manifest/impo.png',
         type: 'image/png',
         sizes: '512x512',
         purpose: 'maskable',
       },
       {
-        src: '/assets/manifest/impo.webp',
+        src: '/assets/manifest/impo.png',
         type: 'image/png',
         sizes: '512x512',
         purpose: 'any',
       },
       {
-        src: '/assets/manifest/impo.webp',
+        src: '/assets/manifest/impo.png',
         type: 'image/png',
         sizes: '192x192',
       },
       {
-        src: '/assets/manifest/impo.webp',
+        src: '/assets/manifest/impo.png',
         type: 'image/png',
         sizes: '180x180',
       },
       {
-        src: '/assets/manifest/impo.webp',
+        src: '/assets/manifest/impo.png',
         type: 'image/png',
         sizes: '152x152',
       },
       {
-        src: '/assets/manifest/impo.webp',
+        src: '/assets/manifest/impo.png',
         type: 'image/png',
         sizes: '120x120',
       },
