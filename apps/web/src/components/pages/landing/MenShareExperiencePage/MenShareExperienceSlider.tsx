f46274b6ec3e2a6231c42c498dc00d14dce90a92
NazaninShahidi <nshahidi461@gmail.com>
2026-05-10 08:59:08 +0330

    fix bg

--- apps/web/src/components/pages/landing/MenShareExperiencePage/MenShareExperienceSlider.tsx
+++ apps/web/src/components/pages/landing/MenShareExperiencePage/MenShareExperienceSlider.tsx
@@ -12,7 +12,10 @@ const MenShareExperienceSlider = () => {
     <CustomSlider gap={16} className="!flex-row px-3 lg:px-0 md:px-0">
       {MENEXPERIENCE_IMAGES.map((item, index) => {
         return (
-          <div className="relative min-w-[280px] h-[428px] w-full  bg-impo_Neutral_Surface rounded-2xl" key={index}>
+          <div
+            className="relative min-w-[280px] lg:h-[428px] h-[345px] w-full  bg-impo_Neutral_Surface rounded-2xl"
+            key={index}
+          >
             <CustomImage
               src={item.image}
               alt={`Experience ${index + 1}`}
@@ -22,13 +25,13 @@ const MenShareExperienceSlider = () => {
               sizes="100vw"
             />
             <div
-              className="px-4 absolute -bottom-4 left-0 right-0 flex flex-col justify-center h-[167px] items-right gap-1 w-full
-               bg-[linear-gradient(180deg,rgba(249,249,249,0.2)_0%,#F9F9F9_49.76%)] dark:bg-[linear-gradient(180deg,rgba(0,0,0,0.2)_0%,#2a2a2a_60%)] "
+              className="px-4 absolute -bottom-4 left-0 right-0 flex flex-col justify-center lg:h-[207px] h-[167px] items-right gap-1 w-full
+               bg-[linear-gradient(180deg,#F9F9F920_0%,#F9F9F9_49.76%)] dark:bg-[linear-gradient(180deg,rgba(42,42,42,0)_0%,rgba(42,42,42,0.3)_30%,rgba(42,42,42,0.7)_60%,#2a2a2a_100%)]"
               style={{
-                borderRadius: '0px 0px 12.9293px 12.9293px',
+                borderRadius: '0px 0px 16px 16px',
               }}
             >
-              <CustomTypography fontSize="Lable_Large" className="text-impo_Black">
+              <CustomTypography fontSize="Lable_Large" className="text-impo_Neutral_OnBackground">
                 {item.title}
               </CustomTypography>
               <CustomTypography fontSize="Body_Small" className="text-impo_Surface_OnSurfaceVariant text-right ">
@@ -43,11 +46,3 @@ const MenShareExperienceSlider = () => {
 };
 
 export default MenShareExperienceSlider;
-/* Rectangle 5589 */
-
-// position: absolute;
-// width: 240px;
-// height: 167.27px;
-
-// background: linear-gradient(180deg, rgba(249, 249, 249, 0.2) 0%, #F9F9F9 49.76%);
-// border-radius: 0px 0px 12.9293px 12.9293px;
